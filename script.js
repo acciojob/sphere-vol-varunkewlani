1892899f9eb7635780b78778172af1db3bdb4088
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-09 22:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function volume_sphere() {
+let a=document.getElementById("radius")
+let b=document.getElementById("volume")
+let v=0;
+function volume_sphere(event) {
+	event.preventDefault();
+	v=(4/3)* Math.PI * Math.pow(a.value,3)
+	if(isNaN(a.value) || a.value<0)
+	{
+		b.value="NaN"		
+	}
+	
+	if(a.value>0)
+	{		
+		b.value= v.toFixed(4)
+	}
+	
+	
+	
     //Write your code here
   
 } 
